a242b3c44d73876109d5dfa9b24e569b167ba6c3
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2020-07-07 13:07:00 +0100

    Tweaks to colors for bash and zsh

--- profile.d/prompt.sh
+++ profile.d/prompt.sh
@@ -3,12 +3,12 @@ export PS1='\n\t\n\W\n\$ '
 
 # Simple color prompt
 if [ -n "$IS_COLOR" ]; then
-  export PS1='\n\t\n\[\033[${COLOR_BLUE}\]\W\[\033[${COLOR_RESET}\]\n\$ '
+  export PS1='\n\t\n\[\033[${COLOR_BLUE}\]\W\[\033[${COLOR_RESET}\]\n\033[${COLOR_BLUE}\$\033[${COLOR_RESET} '
 fi
 
 # Fancy color prompt
 if [ -n "$IS_COLOR" ] && [ -n "$IS_INTERACTIVE" ]; then
-  export PS1='\n\t$(_prompt_error)\n\[\033[${COLOR_BLUE}\]\W\[\033[${COLOR_RESET}\]$(_prompt_git_branch)\n\$ '
+  export PS1='\n\t$(_prompt_error)\n\[\033[${COLOR_BLUE}\]\W\[\033[${COLOR_RESET}\]$(_prompt_git_branch)\n\033[${COLOR_BLUE}\$\033[${COLOR_RESET} '
 fi
 
 _prompt_git_branch() {
@@ -16,7 +16,7 @@ _prompt_git_branch() {
   local branch="$(git symbolic-ref --short HEAD)"
   local modified="$(git status --porcelain | wc -l | tr -d '[:space:]')"
   printf " ⌥ \033[${COLOR_GREEN}${branch}\033[${COLOR_RESET}"
-  [ "$modified" -gt 0 ] && echo " (\033[${COLOR_RED}${modified}\033[${COLOR_RESET})"
+  [ "$modified" -gt 0 ] && printf " (\033[${COLOR_RED}${modified}\033[${COLOR_RESET})"
 }
 
 _prompt_error() {
--- zshrc
+++ zshrc
@@ -30,5 +30,11 @@ export ZSH_TMUX_AUTOQUIT='false'
 
 [ -n $IS_INTERACTIVE ] && [ -r $ZSH/oh-my-zsh.sh ] && source $ZSH/oh-my-zsh.sh
 
+# Reapply LS_COLORS
+if [ -n "$IS_COLOR" ]; then
+  export LSCOLORS="exfxcxdxbxegedabagacad"
+  export LS_COLORS='di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+fi
+
 # Disable auto `cd`
 unsetopt autocd
