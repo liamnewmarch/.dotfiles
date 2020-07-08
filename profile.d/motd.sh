cecbecd3e5aa02c7997a60dd8715298bc73e3478
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2020-07-08 09:03:46 +0100

    Improvements to motd

--- profile.d/motd.sh
+++ profile.d/motd.sh
@@ -11,5 +11,18 @@ motd() {
       break
     fi
   done
-  echo -e "New $(red 'session') as $(blue "$USER") on $(purple "$name")"
+  local session='session'
+  local user=${USER:-'user'}
+
+  printf "New $(red "$session") as $(blue "$user") on $(purple "$name")\n"
+
+  if [ -n "$IS_INTERACTIVE" ] && [ -z "$TMUX" ]; then
+    local num_tmux="$(_tmux_num_sessions)"
+    [ -z "$num_tmux" ] || [ $num_tmux -eq 0 ] && return
+    if [ $num_tmux -eq 1 ]; then
+      printf "\n* There is $(blue '1') active tmux session\n"
+    else
+      printf "\n* There are $(blue "$num_tmux") active tmux sessions\n"
+    fi
+  fi
 }
--- profile.d/tmux.sh
+++ profile.d/tmux.sh
@@ -1,12 +1,3 @@
-_tmux_list_sessions() {
-  local sessions="$(tmux start-server \; list-sessions)"
-  [ -z "$sessions" ] && return
-  local num="$(echo $sessions | wc -l | tr -d '[:space:]')"
-  if [ "$num" -eq 1 ]; then
-    echo "\n* There is $(blue "$num") active tmux session.\n"
-  else
-    echo "\n* There are $(blue "$num") active tmux sessions.\n"
-  fi
+_tmux_num_sessions() {
+  tmux start-server \; list-sessions | wc -l | tr -d '[:space:]'
 }
-
-[ -n "$IS_INTERACTIVE" ] && [ -z "$TMUX" ] && _tmux_list_sessions
