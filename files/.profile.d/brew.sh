bc723f3eee5256bca1332de3b7b2725a5b96ecfa
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2020-12-06 13:39:45 +0000

    Add continue-on-error option to brew-update function

--- files/.profile.d/brew.sh
+++ files/.profile.d/brew.sh
@@ -11,24 +11,24 @@ brew_update() {
   # If there was a non-zero exit status offer to fix paths
   # shellcheck disable=SC2181
   if [ $? -ne 0 ]; then
-    while true; do
-      printf '\n%s\n' "$(yellow 'Warning:') $(white 'non-zero exit status from') $(magenta 'brew doctor')"
-      printf 'Attempt to fix homebrew paths? [Y/n] '
-      read -r yn
-      case $yn in
-        [Yy]*|'' )
-          # Warn the user we need to use sudo
-          printf '%s\n' "$(white 'Claiming ownership of paths in') $(blue "/usr/local/") (requires sudo)"
-          echo "$HOMEBREW_PATHS" | xargs -n 1 sudo chown -R "$(whoami)"
-          echo "$HOMEBREW_PATHS" | xargs -n 1 chmod u+w
-          break
-          ;;
-        [Nn]*|* )
-          printf '%s\n' "$(red 'Aborted')"
-          exit 0
-          ;;
-      esac
-    done
+    printf '\n%s\n' "$(yellow 'Warning:') $(white 'non-zero exit status from') $(magenta 'brew doctor')"
+    printf 'Attempt to fix homebrew paths? [Y/n/q] '
+    read -r yn
+    case $yn in
+      [Yy]*|'' )
+        # Warn the user we need to use sudo
+        printf '%s\n' "$(white 'Claiming ownership of paths in') $(blue "/usr/local/") (requires sudo)"
+        echo "$HOMEBREW_PATHS" | xargs -n 1 sudo chown -R "$(whoami)"
+        echo "$HOMEBREW_PATHS" | xargs -n 1 chmod u+w
+        ;;
+      [Nn]* )
+        printf '%s\n' "$(white 'Continuing')"
+        ;;
+      [Qq]*|* )
+        printf '%s\n' "$(red 'Aborted')"
+        return
+        ;;
+    esac
   fi
 
   # Run the main brew update command
