dd1b89c34d21857d9e9a9c2d9ccedcb0192de91b
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2021-05-06 10:48:53 +0100

    python: remove homebrew specific cruft

--- files/.profile.d/python.sh
+++ files/.profile.d/python.sh
@@ -2,23 +2,3 @@ export PYTHONDONTWRITEBYTECODE='true'
 export VIRTUALENVWRAPPER_PYTHON='/usr/local/bin/python3'
 
 alias serve='python3 -m http.server'
-
-# Add local python bin dirs for pip-installed things on MacOS.
-if [ -d "$HOME/Library/Python" ]; then
-  for python_path in "$HOME/Library/Python/"*; do
-    if [ -d "$python_path/bin" ]; then
-      export PATH="$python_path/bin:$PATH"
-    fi
-  done
-  unset python_path
-fi
-
-# Load optional Homebrew aliases (python -> python3, pip -> pip3, etc).
-# This may break things!
-if [ -e "$(command -v brew)" ]; then
-  opt_path="$(brew --prefix)/opt/python/libexec/bin"
-  if [ -d "$opt_path" ]; then
-    export PATH="$opt_path:$PATH"
-  fi
-  unset opt_path
-fi
