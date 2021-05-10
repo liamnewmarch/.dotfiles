f100dc1a8bdb382a7b84c35a15f1ebb3174051f0
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2021-05-10 16:24:29 +0100

    npm: use wrapper function for quick private package template

--- files/.profile.d/npm.sh
+++ files/.profile.d/npm.sh
@@ -1,1 +1,26 @@
-alias npminit='printf "{\n  \"private\": true\n}\n" > package.json'
+_npm_init_private_template='{
+  "private": true,
+  "type": "module"
+}
+'
+
+npm() {
+  case "$1" in
+    init )
+      printf 'Use quick private package template? [Y/n] '
+      read -r yn
+      case "$yn" in
+        [Yy]* | '' )
+          echo "$_npm_init_private_template" > package.json
+          ;;
+        * )
+          command npm "$@"
+          ;;
+      esac
+      ;;
+    * )
+      command npm "$@"
+      ;;
+  esac
+  return $?
+}
